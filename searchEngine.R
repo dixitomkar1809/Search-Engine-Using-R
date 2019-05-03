ebb0d05f83b94deb487d8f4eaed7ec0b1bfa111c
dixitomkar1809 <dixitomkar1809@gmail.com>
2019-05-02 19:34:33 -0500

    Searches are working need to make sure that only top ten results are being returned

--- searchEngine.R
+++ searchEngine.R
@@ -5,15 +5,14 @@ library(tidytext)
 library(wordcloud)
 library(SnowballC)
 
-## Create corpus
-df <- read.table("http://www.utdallas.edu/~ond170030/data/MovieSummaries/plot_summaries.txt", header = FALSE, sep="\t",stringsAsFactors = FALSE,quote="")
+df <- read.table("http://www.utdallas.edu/~ond170030/data/MovieSummaries/plot_summaries_500.txt", header = FALSE, sep="\t",stringsAsFactors = FALSE,quote="")
 df_title <- data.frame(doc_id = df[,1], text = df[,2])
 
 plot.text.list <- as.list(as.character(df_title$text))
-
+no.plot.text.list <- length(plot.text.list)
 names(plot.text.list) <- df_title$doc_id
 
-searchString <- "Thriller Brad Pitt"
+searchString <- "serial killer"
 
 documents <- VectorSource(c(plot.text.list, searchString))
 documents$Names <- c(names(plot.text.list), "searchString")
@@ -30,3 +29,28 @@ cleanCorpus <- tm_map(cleanCorpus, removeNumbers)
 dtm <- DocumentTermMatrix(cleanCorpus)
 
 dtm.tf_idf <- weightTfIdf(dtm)
+
+m<-(as.matrix(dtm.tf_idf))
+
+Filt_m <-m[,m[8,]>0]
+
+CosineCoeff<-0*(1:no.plot.text.list)
+
+for (i in 1:no.plot.text.list) {
+  CosineCoeff[i]<-sum(Filt_m[i,]*Filt_m[8,])/(sum(Filt_m[i,]^2)^0.5 *sum(Filt_m[8,]^2)^0.5)
+}
+
+PageRank<-CosineCoeff[order(CosineCoeff,decreasing=TRUE)]
+RankedPages<-plot.text.list[order(CosineCoeff,decreasing=TRUE)]
+
+EuclCoeff<-0*(1:no.plot.text.list)
+
+for (i in 1:no.plot.text.list) {
+  EuclCoeff[i]<-sum((Filt_m[i,]-Filt_m[8,])^2)^0.5
+}
+
+order(EuclCoeff)
+PageRank<-EuclCoeff[order(EuclCoeff)]
+RankedPages<-plot.text.list[order(EuclCoeff)]
+PageRank
+RankedPages
